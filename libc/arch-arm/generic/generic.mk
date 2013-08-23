1f58136cf46c259088ba25d4f45f81c6976a3e13
nvertigo <ingo@tipanet.dyndns.org>
2013-08-23 11:14:19 +0200

    add NEON optimized memmove and force it in generic

--- libc/arch-arm/generic/generic.mk
+++ libc/arch-arm/generic/generic.mk
@@ -2,7 +2,11 @@ $(call libc-add-cpu-variant-src,MEMCPY,arch-arm/generic/bionic/memcpy.S)
 $(call libc-add-cpu-variant-src,MEMSET,arch-arm/generic/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCAT,string/strcat.c)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/generic/bionic/strcmp.S)
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+$(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/bionic/memmove.S)
+else
 $(call libc-add-cpu-variant-src,MEMMOVE,bionic/memmove.c.arm)
 $(call libc-add-cpu-variant-src,BCOPY,string/bcopy.c.arm)
+endif
 $(call libc-add-cpu-variant-src,STRCPY,arch-arm/generic/bionic/strcpy.S)
 $(call libc-add-cpu-variant-src,STRLEN,arch-arm/generic/bionic/strlen.c)
